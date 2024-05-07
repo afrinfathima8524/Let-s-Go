4cc235e640ee992943f65e6e8f90b5c4a7bc0d3e
Siddhu6801 <Siddharth@blueoshan.com>
2024-05-07 16:58:48 +0530

    Recent

--- lib/features/favourite/ui/widgets/favourite_list_item.dart
+++ lib/features/favourite/ui/widgets/favourite_list_item.dart
@@ -5,17 +5,22 @@ import 'package:lets_go/features/details/ui/screens/details_page.dart';
 import 'package:lets_go/features/favourite/bloc/favourite_bloc.dart';
 import 'package:lets_go/model/places.dart';
 
-class FavouritesSection extends StatelessWidget {
+class FavouritesSection extends StatefulWidget {
   final List<PlacesDataModel> favoriteItems;
 
   const FavouritesSection({Key? key, required this.favoriteItems}) : super(key: key);
 
   @override
+  State<FavouritesSection> createState() => _FavouritesSectionState();
+}
+
+class _FavouritesSectionState extends State<FavouritesSection> {
+  @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemCount: favoriteItems.length,
+      itemCount: widget.favoriteItems.length,
       itemBuilder: (context, index) {
-        final favoriteItem = favoriteItems[index];
+        final favoriteItem = widget.favoriteItems[index];
         return Padding(
           padding: const EdgeInsets.symmetric(vertical: 8.0),
           child: ListTile(
