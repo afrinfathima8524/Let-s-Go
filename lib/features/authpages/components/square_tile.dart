dffc0d26122758ed8567ad3242caee1c9387b55c
afrinfathima8524 <134395860+afrinfathima8524@users.noreply.github.com>
2024-05-24 18:05:57 +0530

    done siginin with google

--- lib/features/authpages/components/square_tile.dart
+++ lib/features/authpages/components/square_tile.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
 
+// ignore: must_be_immutable
 class SquareTile extends StatelessWidget {
   final String imagepath;
-  const SquareTile({super.key, required this.imagepath});
+  Function()? onTap;
+  SquareTile({super.key, required this.imagepath,required this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(20),
-      decoration: BoxDecoration(border: Border.all(color: Colors.white),borderRadius: BorderRadius.circular(16),color: Colors.grey.shade200),
-      child:Image.asset(imagepath,height: 40,),
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: EdgeInsets.all(20),
+        decoration: BoxDecoration(border: Border.all(color: Colors.white),borderRadius: BorderRadius.circular(16),color: Colors.grey.shade200),
+        child:Image.asset(imagepath,height: 40,),
+      ),
     );
   }
 }
