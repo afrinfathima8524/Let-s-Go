1d9ee88eb3f03f80df1e23080a036271eb7ce85c
afrinfathima8524 <134395860+afrinfathima8524@users.noreply.github.com>
2024-06-07 15:19:12 +0530

    done unit testing for api fetching data using mocktail

--- lib/features/home/repo/places_repo.dart
+++ lib/features/home/repo/places_repo.dart
@@ -3,23 +3,28 @@ import 'package:http/http.dart' as http;
 import 'package:lets_go/model/Places.dart';
 
 class PlacesRepo {
-  static Future<List<PlacesDataModel>> fetchPlace({String? query})async{
-    var client=http.Client();
-    List<PlacesDataModel> places=[];
-    try{
-      var response= await client.get(Uri.parse('https://mocki.io/v1/eecf478e-90fe-4beb-b869-22562682fd89'));
+  final http.Client client;
+  PlacesRepo(this.client);
+  Future<List<PlacesDataModel>> fetchPlace({String? query}) async {
+    List<PlacesDataModel> places = [];
+    try {
+      var response = await client.get(Uri.parse(
+          'https://mocki.io/v1/eecf478e-90fe-4beb-b869-22562682fd89'));
       List result = jsonDecode(response.body);
       for (var i = 0; i < result.length; i++) {
-        PlacesDataModel place=PlacesDataModel.fromJson(result[i] as Map<String,dynamic>);
+        PlacesDataModel place =
+            PlacesDataModel.fromJson(result[i] as Map<String, dynamic>);
         places.add(place);
         if (query != null) {
-          places=places.where((element) => element.name!.toLowerCase().contains(query.toLowerCase())).toList();
+          places = places
+              .where((element) =>
+                  element.name!.toLowerCase().contains(query.toLowerCase()))
+              .toList();
         }
       }
       return places;
+    } catch (e) {
+      return [];
     }
-    catch(e){
-      return[]
-;    }
   }
 }
